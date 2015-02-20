a019ad2a4fff6d88fa201fa4bfca89a7e5c1e545
Johan Brook <johan.jvb@gmail.com>
2015-02-20 04:12:54 +0100

    Version 0.1.0

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'lookback:body-class',
-  version: '0.0.2',
+  version: '0.1.0',
   git: 'https://github.com/lookback/meteor-bodyclass',
   summary: 'Automatically add classes for Iron Router templates and layouts for scoping with CSS.'
 });
